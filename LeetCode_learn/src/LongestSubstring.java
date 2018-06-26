860ec06391a72a881ed729ad60464ea21f9da987
wxyu <540690106@qq.com>
2018-06-26 20:26:57 +0800

    LongestSubstring

--- LeetCode_learn/src/LongestSubstring.java
+++ LeetCode_learn/src/LongestSubstring.java
@@ -1,6 +1,4 @@
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author: admin
@@ -8,7 +6,40 @@ import java.util.Set;
  * @description:
  */
 public class LongestSubstring {
-    public int lengthOfLongestSubstring(String s) {
+
+    public Map<StringBuilder, Integer> LongestSubStringByHashmap(String s){
+        if (s == null)
+            return null;
+        Map<StringBuilder, Integer> result = new HashMap<>(); //result
+        StringBuilder maxSubString = new StringBuilder("");//最长子串
+        int startIndex = -1;//记录每次获取不同子字符串开始的位置
+        int oriStartIndex = startIndex;//记录最终最长子串开始的位置
+        int maxLen = 0;
+        char[] strs = s.toCharArray();
+        int[] charsIndex = new int[256];//serve every character index
+
+        for (int i = 0 ; i < 256 ; i++)//保存每隔字符的位置
+            charsIndex[i] = -1;
+        for (int index = 0 ; index < strs.length ; index++){
+            char temp = strs[index];
+            if (charsIndex[temp] > -1 && charsIndex[temp] > startIndex){//repeat
+                startIndex = charsIndex[temp];//将开始位置重置成出现重复元素的位置
+            }
+            // index-1 表示重复字符之前的一个位置，例如abcdfgb,这里b重复的时候取的子串是bcdfg
+            if (index - 1 - startIndex + 1 > maxLen){
+                maxLen = index - startIndex;
+                oriStartIndex = startIndex + 1;
+            }
+            charsIndex[temp] = index;//更新字符的位置值
+        }
+        for (int j = oriStartIndex ; j < oriStartIndex + maxLen ; j ++)
+            maxSubString.append(strs[j]);
+        result.put(maxSubString, maxLen);
+        return result;
+
+    }
+
+    public int lengthOfLongestSubstringByHashmap(String s) {
         int n = s.length();
         char temp;
         Set<Character> set = new HashSet<>();
@@ -20,25 +51,27 @@ public class LongestSubstring {
             if (!set.contains(s.charAt(j))){
                 temp = s.charAt(j++);
                 set.add(temp);
-                Iterator<Character> it = set.iterator();
-                while (it.hasNext()){
-                    res.append(temp);
-                    it.next();
-                }
                 ans = Math.max(ans, j - i);
             }
             else {
                 set.remove(s.charAt(i++));
             }
         }
-        System.out.println(res);
         return ans;
     }
 
     public static void main(String[] args) {
-        String s = "abcabcdd";
+        String s = "jhhjnsfudufbdfyscfbsdjjSAFASFsefyrsjksaudhsduhasdbg" +
+                "ywqep[m,mzaASFASFhuwenzsdjsjfasfyaehwzsjx ;ASFASFlpwisjd" +
+                "asuwnad.pqwekASFqowe92347174nsd sdfsdf73bwASFawue821b9sa" +
+                "dasjdnasdqASFASASFF2en128adasjdnqwudSAASFF ASFw 1wq89ewa" +
+                "dasjdASFhqw8edeqwhedhqwueASFuquweuqwuehqw e123xrkASFajs8" +
+                "9da2qe54we24eDASASAFFdfsdifhsd";
+                //"abcabcdd";
         LongestSubstring test = new LongestSubstring();
-        int res = test.lengthOfLongestSubstring(s);
-        System.out.println(res);
+        int res = test.lengthOfLongestSubstringByHashmap(s);
+        Map<StringBuilder, Integer> map = test.LongestSubStringByHashmap(s);
+        System.out.println(map.entrySet());
+        System.out.print(res);
     }
 }
