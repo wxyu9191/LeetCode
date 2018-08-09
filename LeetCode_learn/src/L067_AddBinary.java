095b59349dd37edc4037f42ad8a6b201a817f002
wxyu9191 <540690106@qq.com>
2018-08-09 22:50:12 +0800

    normal

--- LeetCode_learn/src/L067_AddBinary.java
+++ LeetCode_learn/src/L067_AddBinary.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
+/*
+@author: bass
+@data: 2018-08-08 08:04:40
+*/
 public class L067_AddBinary {
+    public String addBinary(String a, String b){
+        StringBuilder res = new StringBuilder();
+        int i = a.length() - 1, j = b.length() - 1, carry = 0;
+        while (i >=0 || j >= 0){
+            int sum  = carry;
+            if ( j >= 0) sum += b.charAt(j--) - '0';
+            if ( i >= 0) sum += a.charAt(i--) - '0';
+            res.append(sum % 2);
+            carry = sum/2;
+        }
+        if (carry != 0)
+            res.append(carry);
+        return res.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        L067_AddBinary test = new L067_AddBinary();
+        System.out.println(test.addBinary("101", "1010"));
+    }
 }
--- LeetCode_learn/src/SwordOffer/Singleton1.java
+++ LeetCode_learn/src/SwordOffer/Singleton1.java
@@ -1,4 +1,45 @@
 package SwordOffer;
 
+/*
+@author: bass
+@Data: 2018-08-06 08:21:20
+*/
+
 public class Singleton1 {
+    //no lock, only fit single thread
+    private Singleton1(){}
+    private static Singleton1 instance = null;
+    public static Singleton1 getInstance(){
+        if (instance == null)
+            instance = new Singleton1();
+        return instance;
+    }
+
+    //lock the whole block, low efficiency
+    private static Singleton1 instance_bad_lock = null;
+    public static Singleton1 getInstance_bad_lock(){
+        if (instance_bad_lock == null){
+            synchronized (Singleton1.class){
+                instance_bad_lock = new Singleton1();
+            }
+        }
+        return instance_bad_lock;
+    }
+
+    //利用静态构造函数，不过会过早的创建实例，降低内存的使用效率。
+    private static Singleton1 instance_good = new Singleton1();
+    public static Singleton1 getInstance_good(){
+        return instance_good;
+    }
+
+
+    private static Singleton1 instace_best;
+    public Singleton1 getInstace_best(){
+        return Nestd.instance_test;
+    }
+    private static class Nestd{
+         static Singleton1 instance_test = new Singleton1();
+    }
+
+
 }
